ec061a01c07b329a7a54d04680938a5224b8ee2f
Your Name <syhuy851@gmail.com>
2021-11-04 13:51:22 +0700

    5th commit

--- src/components/WhatHappened.vue
+++ src/components/WhatHappened.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative">
-       <div class="absolute hidden md:block left-56 top-0">
+       <div class="absolute  left-226 top-0">
           <img src="@/assets/whpn/video-object-05.png" alt="">
       </div>
       <div class="text-whpn-title font-extrabold tracking-5px uppercase text-center pt-36 mb-12">
@@ -25,16 +25,16 @@ every team makes a lot of videos Can be trimmed. Take the first
           >SEE MORE</a
         >
       </div>
-       <div class="absolute hidden md:block -left-80 top-80">
+       <div class="absolute hidden md:block -left-80 top-314">
           <img src="@/assets/whpn/video-object-04.png" alt="">
       </div>
-        <div class="absolute  top-64 right-60  z-20">
+        <div class="absolute  top-270 right-223 z-20">
           <img src="@/assets/whpn/video-object-06.png" alt="">
       </div>
-        <div class="absolute  top-40 right-36  z-10">
+        <div class="absolute  top-168 right-150  z-10">
           <img src="@/assets/whpn/video-object-07.png" alt="">
       </div>
-       <div class="absolute hidden md:block -right-52 bottom-60  z-20">
+       <div class="absolute hidden md:block -right-52 bottom-242  z-20">
           <img src="@/assets/whpn/video-object-08.png" alt="">
       </div>
         <div class="absolute hidden md:block -right-44 top-656 bottom-60  z-10">
@@ -43,13 +43,13 @@ every team makes a lot of videos Can be trimmed. Take the first
       <div class="ml-2 pb-96 relative z-50">
           <img src="@/assets/whpn/video.png" class="block mx-auto z-50" alt="">
       </div>
-       <div class="absolute  bottom-36 left-80  z-30">
+       <div class="absolute  bottom-147 left-348  z-30">
           <img src="@/assets/whpn/video-object-01.png" alt="">
       </div>
-       <div class="absolute  bottom-72 right-656  z-20">
+       <div class="absolute  bottom-276 right-648  z-20">
           <img src="@/assets/whpn/video-object-02.png" alt="">
       </div>
-       <div class="absolute bottom-80 left-60  z-20">
+       <div class="absolute bottom-314 left-223  z-20">
           <img src="@/assets/whpn/video-object-03.png" alt="">
       </div>
       
